1034f1fd7477caa84f095cf6afce7703ed9ba119
trueutkarsh <iamutkash@cse.iitb.ac.in>
2017-01-20 11:24:39 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This repository contain programming lab assignments for the course
+## Implementation of Programming Languages
+
+##Done
+* ~~Lab1~~
---
+++ cs316-asgn1/test.sh
@@ -0,0 +1,17 @@
+make
+rm -rf Examples16
+mkdir Examples16
+rm -rf Examples/*.spim
+cp Examples/* Examples16/
+for i in `seq 1 7`;
+do
+	./sclp "Examples/test$i.c"
+	./sclp16 "Examples16/test$i.c"
+	diff "Examples/test$i.c.spim" "Examples16/test$i.c.spim" &> /dev/null
+	if [ $? = 0 ];
+	then
+		echo "Test Case $i alright"
+	else
+		echo "OOPS! wrong for test case $i"
+	fi
+done
